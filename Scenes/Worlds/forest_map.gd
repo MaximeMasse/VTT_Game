ea402e53c35552fd9b0c5c296d2620c8605133ee
Zigggggyyy <maxime.masse@gmail.com>
2026-06-16 23:41:08 +0200

    getting closer

--- Scenes/Worlds/forest_map.gd
+++ Scenes/Worlds/forest_map.gd
@@ -12,12 +12,16 @@ var running
 func _ready():
 	get_tree().debug_collisions_hint = false
 	running = false
+	set_up_button(%ToVillageButton)
+	set_up_button(%Desert)
+	%Desert.hide()
 	# Ref names, nodes and zones, create outline.
 	# Node init, valid and stars, naming
 	for node in %Nodes.get_children():
 		var node_data : Dictionary = {"node":node,"zone_out":[]}
 		# Maps
 		if node is TextureButton:
+			set_up_button(node)
 			node.disabled = true
 			node_data["done"] = node.get_child(0)
 			node_data["valid"] = node.get_child(1)
@@ -32,8 +36,7 @@ func _ready():
 		# Path
 		world_datas["Zones"][zone]["path"] = zone.get_child(1)
 		# Has Lock
-		world_datas["Zones"][zone]["has_lock"] = true if zone.get_child(1).get_child_count()!=0\
-		and zone.get_child(1).get_child(0).name == "Lock" else false
+		world_datas["Zones"][zone]["has_lock"] = true if zone.get_child_count() == 3 else false
 		# Outline
 		world_datas["Zones"][zone]["outline"] = create_outline(zone)
 		# In and out
@@ -44,13 +47,18 @@ func _ready():
 	set_player_datas()
 	# Update world
 	update_world()
+	# Current node new paths and active zone
+	draw_new_paths()
+	# Active zones
+	set_active_zones()
 	# Debug print
 	print("Nodes :")
 	for node in world_datas["Nodes"]:print(node," : ",world_datas["Nodes"][node])
 	print("\nZones :")
 	for zone in world_datas["Zones"]:print(zone," : ",world_datas["Zones"][zone])
 	print("\nPlayer datas : ",player_datas)
-
+	print("\nActives zones : ",active_zones)
+	running = true
 
 func set_player_datas():
 	player_datas["finished_maps"] = []
@@ -103,11 +111,71 @@ func update_world():
 			# Anim
 			if "To" not in node or node in player_datas["unlocks"]:world_datas["Zones"][node_datas["zone_out"][0]]["path"].unlock()
 			# Path to final chairlift to see if previous map done
-			#if "To" in node
+			if "To" in node:
+				# Previous map seen this run
+				if path_in.name.split("_")[0] in player_datas["run_finished_maps"]:
+					path_in.show()
+					path_in.get_child(0).hide()
+					path_in.modulate = Color(0.5, 0.5, 0.5, 1.0)
+				# Previous map seen on previous run
+				elif path_in.name.split("_")[0] in player_datas["finished_maps"]:
+					path_in.show()
+					path_in.get_child(0).hide()
+					path_in.modulate = Color(0.0, 0.0, 0.0, 1.0)
+
+func draw_new_paths():
+	print("\ndraw_new_paths :\nCurrent node : ",player_datas["node"])
+	# Out path of current node
+	for zone in world_datas["Nodes"][player_datas["node"]]["zone_out"]:
+		var destination : String = zone.name.split("_")[1] 
+		# Origin or destination not chairlift
+		if "Chairlift" not in zone.name:
+			# Destination never seen
+			if destination not in player_datas["finished_maps"]:print("draw never seen path to : ",destination)
+			# Destination never seen this run
+			elif destination not in player_datas["run_seen_maps"]: print("draw never seen this run path to : ",destination)
+
+func set_active_zones():
+	world_datas["Nodes"][player_datas["node"]]["node"].modulate = Color(1.0, 1.0, 1.0, 1.0)
+	for zone in world_datas["Nodes"][player_datas["node"]]["zone_out"]:
+		active_zones[zone] = false if world_datas["Zones"][zone]["has_lock"] and\
+					zone.name.split("_")[1] not in player_datas["unlocks"] else true
+
+func on_zone_hover(zone:Area2D):
+	AudioManager.play_ui("map_hover")
+	# Regular zone
+	if active_zones[zone] :
+		world_datas["Nodes"][zone.name.split("_")[1]]["node"].modulate = Color(1.0, 1.0, 1.0, 1.0)
+		world_datas["Zones"][zone].modulate = Color(1.0, 1.0, 1.0, 1.0)
+		world_datas["Zones"][zone]["path"].modulate = Color(1.0, 1.0, 1.0, 1.0)
+		world_datas["Zones"][zone]["outline"].show()
+	# Locked zone
+	else :
+		world_datas["Zones"][zone].modulate = Color(0.5, 0.5, 0.5, 1.0)
+		world_datas["Zones"][zone]["outline"].modulate = Color(0.5, 0.5, 0.5, 1.0)
+		world_datas["Zones"][zone]["outline"].show()
+func on_zone_exit(zone:Area2D):
+		world_datas["Nodes"][zone.name.split("_")[1]]["node"].modulate = Color(0.5, 0.5, 0.5, 1.0)
+		world_datas["Zones"][zone].modulate = Color(0.5, 0.5, 0.5, 1.0)
+		if "Chairlift" not in zone.name.split("_")[0]:world_datas["Zones"][zone]["path"].modulate = Color(0.5, 0.5, 0.5, 1.0)
+		world_datas["Zones"][zone]["outline"].hide()
+func on_zone_click(zone:Area2D):
+	if active_zones[zone] :
+		AudioManager.play_ui("click")
+		active_zones = {}
+		running = false
+		var origin : String = zone.name.split("_")[0] 
+		var destination : String = zone.name.split("_")[1]
+		world_datas["Nodes"][destination]["node"].modulate = Color(1.0, 1.0, 1.0, 1.0)
+		if "Chairlift" in origin:
+			world_datas["Zones"][zone]["path"].play()
+			await get_tree().create_timer(3).timeout
+			if "To" in origin:%Desert.show()
+			else:world_datas["Nodes"][destination]["node"].disabled = false
+		else:pass
+	else:AudioManager.play_ui("lock")
 		
-func on_zone_hover(zone:Area2D):pass
-func on_zone_exit(zone:Area2D):pass
-func on_zone_click(zone:Area2D):pass
+	
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
@@ -135,6 +203,7 @@ func create_outline(zone:Area2D) -> Line2D :
 	# Style
 	outline.width = 8
 	outline.closed = true
+	outline.joint_mode = Line2D.LINE_JOINT_ROUND
 	outline.visible = false
 	# Copying
 	var index : int = 0
@@ -143,8 +212,24 @@ func create_outline(zone:Area2D) -> Line2D :
 		index += 1
 	return outline
 
+func draw_path(path:Line2D,duration:float):
+	var path_points : PackedVector2Array = path.points
+	var copy_line := Line2D.new()
+	path.get_parent().add_child(copy_line)
+	var drawing_frequency : float = duration/path_points.size()
+	# Style copy
+	copy_line.position = path.position
+	copy_line.z_index = path.z_index
+	copy_line.z_as_relative = path.z_as_relative
+	copy_line.width = path.width
+	# Drawing
+	var index : int = 0
+	while index < path_points.size():
+		copy_line.add_point(path_points[index])
+		index += 1
+		await get_tree().create_timer(drawing_frequency).timeout
+
 func set_up_button(node:BaseButton):
-	node.disabled = true
 	node.mouse_entered.connect(func(): on_button_hover(node))
 	node.mouse_exited.connect(func(): on_button_hover_exit(node))
 	node.pressed.connect(func():on_button_pressed(node))
